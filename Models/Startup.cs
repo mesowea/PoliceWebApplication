1490444aa50f420292bf67614da4d81d85407c4f
versadchikov <versadchikov@gmail.com>
2021-03-04 18:08:44 +0200

    added Cities controller

--- Models/Startup.cs
+++ Models/Startup.cs
@@ -54,7 +54,7 @@ namespace PoliceWebApplication
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Cities}/{action=Index}/{id?}");
             });
         }
     }
